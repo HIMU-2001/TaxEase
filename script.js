48273fe55d182986ced3b3ac121d0a036cdb0824
HIMANSHU RAI <himanshuuu.2001@gmail.com>
2025-02-03 10:18:00 +0530

    minor tax slab modification

--- script.js
+++ script.js
@@ -18,7 +18,10 @@ function taxCalculator() {
                 { range: "0 - 4 lakh", rate: "0%", tax: 0 },
                 { range: "4 - 8 lakh", rate: "5%", tax: this.taxableSalary > 400000 ? Math.min(this.taxableSalary - 400000, 400000) * 0.05 : 0 },
                 { range: "8 - 12 lakh", rate: "10%", tax: this.taxableSalary > 800000 ? Math.min(this.taxableSalary - 800000, 400000) * 0.10 : 0 },
-                { range: "Above 12 lakh", rate: "30%", tax: this.taxableSalary > 1200000 ? (this.taxableSalary - 1200000) * 0.30 : 0 },
+                { range: "12 - 16 lakh", rate: "15%", tax: this.taxableSalary > 1200000 ? Math.min(this.taxableSalary - 1200000, 400000) * 0.15 : 0 },
+                { range: "16 - 20 lakh", rate: "20%", tax: this.taxableSalary > 1600000 ? Math.min(this.taxableSalary - 1600000, 400000) * 0.20 : 0 },
+                { range: "20 - 24 lakh", rate: "25%", tax: this.taxableSalary > 2000000 ? Math.min(this.taxableSalary - 2000000, 400000) * 0.25 : 0 },
+                { range: "Above 24 lakh", rate: "30%", tax: this.taxableSalary > 2400000 ? (this.taxableSalary - 2400000) * 0.30 : 0 },
             ];
         },
         get totalTax() {
